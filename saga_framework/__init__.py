6a174f5b9dac9f858184cd22b783048434673646
Oleksandr Lytvynenko <litvinenko1706@gmail.com>
2021-04-22 15:40:39 +0300

    ~ renamed 1 file

--- saga_framework/__init__.py
+++ saga_framework/__init__.py
@@ -1,6 +1,6 @@
 from .base_saga import *
 from .async_saga import *
-from .stateful import *
+from .stateful_saga import *
 from .utils import *
 from .handlers import *
 from .celery_utils import *
